838f8aa309f5f4f7d86cb0d83a3526a21767c99e
ramses urbina <ramsesurb@gmail.com>
2022-09-05 20:43:32 -0300

    desafio itemDetail 4.0

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Header } from './components/Header/Header'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-import {BrowserRouter,Routes,Route} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 
 
@@ -21,9 +21,7 @@ function App() {
 
       <Header/>
       
-       <Routes>
-       <Route path='/item/:itemId' element={<ItemDetailContainer />}/>
-      </Routes>
+      <ItemDetailContainer/>
       <ItemListContainer/>
 
       </BrowserRouter>
---
+++ src/components/Data/Data2.js
@@ -0,0 +1,30 @@
+export const stock = [
+    {
+      id: 1,
+      name: "Fender Player Stratocastor HSS",
+      brand: "Fender",
+      fantasyColor: "Polar White",
+      color: "white",
+      type: "Electric Guitar",
+      category: "guitar",
+      price: 879.99,
+      inStock: true,
+      isTopSeller: true,
+      orientation: "Right",
+      neckWood: "Maple",
+      fretNumber: 22,
+      strings: 6,
+      pickups: {
+        config: "HSS",
+        neck: "Player Series Stratocaster single coil",
+        middle: "Player Series Stratocaster single coil",
+        bridge: "Player Series Stratocaster humbucker"
+      },
+      rating: 5,
+      isOnSale: false,
+      images:"https://muzikercdn.com/uploads/products/789/78991/main_9a486311.jpg", 
+      desc: "Over the decades, players have been continually inspired by the sound of a Strat. From the clarity of the high end, through the gut punch of the mids and the solid lows, it's a sound that's helped define what an electric guitar should be—versatile enough for any style and broad enough for any player to find an individual voice. This Player Series Stratocaster puts all of the classic features of the Strat at your fingertips while adding a modern edge, including a bridge position humbucker and Floyd Rose double-locking tremolo system for a little more rock-and-roll attitude. With a solid alder body, a 22-fret maple fingerboard with a contemporary 9.5' radius for easy bending and the dive-bombing madness of a Floyd Rose, this Player Series Stratocaster HSS is primed and ready to carry you along your musical voyage.",
+      "countryOrigin": "Mexico"
+    },
+    
+]
